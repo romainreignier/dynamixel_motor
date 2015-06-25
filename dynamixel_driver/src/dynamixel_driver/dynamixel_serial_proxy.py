fa2b18849c876306d3e40c456bfb493ddd324cb8
Romain REIGNIER <romain.reignier@cea.fr>
2015-06-25 18:13:53 +0200

    Deleted all queue_size

--- dynamixel_driver/src/dynamixel_driver/dynamixel_serial_proxy.py
+++ dynamixel_driver/src/dynamixel_driver/dynamixel_serial_proxy.py
@@ -91,8 +91,8 @@ class SerialProxy():
         self.current_state = MotorStateList()
         self.num_ping_retries = 5
         
-        self.motor_states_pub = rospy.Publisher('motor_states/%s' % self.port_namespace, MotorStateList, queue_size=1)
-        self.diagnostics_pub = rospy.Publisher('/diagnostics', DiagnosticArray, queue_size=1)
+        self.motor_states_pub = rospy.Publisher('motor_states/%s' % self.port_namespace, MotorStateList)
+        self.diagnostics_pub = rospy.Publisher('/diagnostics', DiagnosticArray)
 
     def connect(self):
         try:
